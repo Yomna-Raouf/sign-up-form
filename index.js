0b27cd5a3ef9672750978c7ba6345c06f4198ab0
yomna raouf <yomnaraouf99@gmail.com>
2020-04-28 01:10:57 -0700

    javascript DONE + made responsive

---
+++ index.js
@@ -0,0 +1,11 @@
+document.querySelector('.banner-btn').addEventListener('click', () => {
+    document.querySelector('.banner').style.display = 'none';
+    document.querySelector('.form-container').style.cssText = 'opacity: 1; visibility: visible';
+    document.querySelector('.container').style.cssText = 'background: #464545 no-repeat center center fixed; background-size: cover';
+});
+
+document.querySelector('.x-btn').addEventListener('click', () => {
+    document.querySelector('.banner').style.display = 'flex';
+    document.querySelector('.form-container').style.cssText = 'opacity: 0; visibility: hidden';
+    document.querySelector('.container').style.cssText = ' background: linear-gradient(rgba(0, 0, 0, .8), rgba(0, 0, 0, .7) ),url(assets/bg1.jpeg) no-repeat center center fixed; background-size: cover';
+});
