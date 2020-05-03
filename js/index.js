d53b67af46f3ab5d5d51e6eca8e968e6f045cc8b
Yomna Raouf <54322994+Yomna-Raouf@users.noreply.github.com>
2020-05-02 23:40:18 -0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -7,5 +7,5 @@ document.querySelector('.banner-btn').addEventListener('click', () => {
 document.querySelector('.x-btn').addEventListener('click', () => {
     document.querySelector('.banner').style.display = 'flex';
     document.querySelector('.form-container').style.cssText = 'opacity: 0; visibility: hidden';
-    document.querySelector('.container').style.cssText = ' background: linear-gradient(rgba(0, 0, 0, .8), rgba(0, 0, 0, .7) ),url(assets/bg1.jpeg) no-repeat center center fixed; background-size: cover';
+    document.querySelector('.container').style.cssText = ' background: linear-gradient(rgba(0, 0, 0, .8), rgba(0, 0, 0, .7) ),url("../assets/bg1.jpeg") no-repeat center center fixed; background-size: cover';
 });
